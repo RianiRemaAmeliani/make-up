97bf7cd6910a45c0136ec78c2a533fb40d1c25ef
RianiRemaAmeliani <93575093+RianiRemaAmeliani@users.noreply.github.com>
2022-03-15 09:59:33 +0700

    Add files via upload

---
+++ src/views/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Contact</h1>
+      <peta />
+    </div>
+    <div class="col-md-12">
+      <fromcontact />
+    </div>
+  </div>
+</template>
+
+<script>
+import peta from '../components/Peta.vue'
+import fromcontact from '../components/fromcontact.vue'
+export default{
+    components: {
+      peta,
+      fromcontact,
+    }
+  }
+</script>
+
+
+
